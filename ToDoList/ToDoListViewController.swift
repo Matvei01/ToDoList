451840376b48b582c7c1ae96dd926029525df482
Matvei95 <khlestov.95@mail.ru>
2022-12-05 10:07:11 +0500

    Working with Core Data

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -6,13 +6,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
-    public var tasks: [String] = []
+    private var tasks: [Task] = []
+    
+    // MARK: - Override Methods
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let context = getContext()
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+//        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+//        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
+    }
+    
+    // MARK: - Private Methods
+    private func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    private func saveTask(with title: String) {
+        
+        let context = getContext()
+        
+        guard let entinty = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        
+        let taskObject = Task(entity: entinty, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
     // MARK: - IB Actions
@@ -24,8 +64,8 @@ class ToDoListViewController: UITableViewController {
         )
         let saveAction = UIAlertAction(title: "Save", style: .default) { action in
             let textFild = alertController.textFields?.first
-            if let newTask = textFild?.text {
-                self.tasks.insert(newTask, at: 0)
+            if let newTaskTitle = textFild?.text {
+                self.saveTask(with: newTaskTitle)
                 self.tableView.reloadData()
             }
         }
@@ -34,8 +74,25 @@ class ToDoListViewController: UITableViewController {
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .default) {_ in }
         
+        let deleteAction = UIAlertAction(title: "Don't save", style: .default) {_ in
+            let context = self.getContext()
+            let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+            if let tasks = try? context.fetch(fetchRequest) {
+                for task in tasks {
+                    context.delete(task)
+                }
+            }
+
+            do {
+                try context.save()
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
+        alertController.addAction(deleteAction)
         
         present(alertController, animated: true)
     }
@@ -56,56 +113,10 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
 
-        cell.textLabel?.text = tasks[indexPath.row]
+        let task = tasks[indexPath.row]
         
-
+        cell.textLabel?.text = task.title
+        
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
